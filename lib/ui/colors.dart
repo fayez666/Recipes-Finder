ebcb0bc7724a78e010bfc8ee4e3742eb575bb159
Abdallah Fayez <77704981+fayez666@users.noreply.github.com>
2023-07-15 13:03:32 +0300

    Handling Shared Preferences

---
+++ lib/ui/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color green = Color(0xff158442);
+const Color lightGrey = Color(0xff5f6367);
+const Color shim = Color(0x7f000000);
+
+const ColorFilter activeIconColor = ColorFilter.mode(Colors.green, BlendMode.srcIn);
+const ColorFilter inActiveIconColor = ColorFilter.mode(Colors.grey, BlendMode.srcIn);
